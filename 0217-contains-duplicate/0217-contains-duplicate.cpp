b331f5913a2ebb18066ad4c3df65a3b6ac30435a
sanyabehl13 <147542656+sanyabehl13@users.noreply.github.com>
2026-04-25 05:42:41 +0530

    Time: 19 ms (95.09%), Space: 71 MB (79.91%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-
+        sort(nums.begin(), nums.end());//so that single loop is used O(nlogn)
+//solvable by hash set
         for (int i = 1; i < nums.size(); i++) {
            //best to check contagious, adjacent elemts//
             if (nums[i] == nums[i - 1])
@@ -11,3 +11,16 @@ public:
         return false;
     }
 };
+/*class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for (int num : nums) {
+            if (seen.count(num))
+                return true;   // already saw this number before
+            seen.insert(num);  // first time seeing it, store it
+        }
+        return false;
+    }
+};*/
